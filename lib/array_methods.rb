3ccbf66e609f6a076b631ad94aad709f4ef2b7ca
vincent servio <vincentservio@gmail.com>
2019-09-05 02:57:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,6 @@
 def find_element_index(array, value_to_find)
-  count = 0 
-  while count < array.length do 
- if array[count] == value_to_find
-    
-    p count 
-end
+array.length.times do |count|
+  
   end  
  count += 1
 end
