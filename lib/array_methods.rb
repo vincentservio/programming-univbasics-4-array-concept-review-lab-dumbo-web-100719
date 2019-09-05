92462030a235e8b304e5fbe5f6b560c45e40c07b
vincent servio <vincentservio@gmail.com>
2019-09-05 03:10:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
 array.length.times do |count|
   if array[count] == value_to_find
-    p count
+    return count
     end 
   nil
  end
@@ -10,7 +10,7 @@ end
 
 
 def find_max_value(array)
- p array.max
+  array.max
 end
 
 def find_min_value(array)
